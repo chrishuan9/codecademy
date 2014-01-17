6c2e50908442290ae97144d9db26dead67c11ce1
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-01-17 23:59:50 +0100

    implemented function for exercise 11

--- python/15PMP-11.py
+++ python/15PMP-11.py
@@ -6,7 +6,12 @@ def count(sequence, item):
     """Let's write a function count that takes a list sequence and a value item as input and returns how many times the item occurs in the list.
 
     For example: count([1,2,1,1], 1) should return 3 (because 1 appears 3 times in the list)."""
-
+    found = 0
+    for entity in sequence:
+        if entity == item:
+            found+=1
+    return found
+    
 
 if __name__ == "__main__":
     main()

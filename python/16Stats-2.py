483c6696d57ccaa8305de99966505425092be9a0
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-01-20 23:56:23 +0100

    added exercise 2 from chapter 16: codecadamys python course

---
+++ python/16Stats-2.py
@@ -0,0 +1,12 @@
+def main():
+  grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+  print_grades(grades)
+
+
+def print_grades(grades):
+	for grade in grades:
+		print grade
+
+
+if __name__ == "__main__":
+    main()

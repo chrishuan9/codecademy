344131884f4116b64cd3ebd42bc3358c7f18650f
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-01-23 01:28:59 +0100

    last exercise to finish the chapter statistics

---
+++ python/16Stats-9.py
@@ -0,0 +1,37 @@
+def main():
+  grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+  print_grades(grades)
+  print grades_sum(grades)
+  print grades_average(grades)
+  print grades_variance(grades,grades_average(grades))
+  print grades_std_deviation(grades_variance(grades,grades_average(grades)))
+
+
+def print_grades(grades):
+	for grade in grades:
+		print grade
+
+		
+def grades_sum(scores):
+	total = 0
+	for grade in scores:
+		total+=grade
+	return total
+
+def grades_average(grades):
+    sum = grades_sum(grades)
+    average = float(sum) / len(grades)
+    return average
+
+def grades_variance(scores,average):
+    variance = 0
+    for grade in scores:
+        variance += (grade - average) ** 2
+    return float(variance) / len(scores)
+
+def grades_std_deviation(variance):
+    return variance ** 0.5
+
+
+if __name__ == "__main__":
+    main()

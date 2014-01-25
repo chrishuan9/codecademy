bcaa957904ff76ab54abb86c3df22adfa586f17f
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-01-25 23:48:58 +0100

    practices makes perfect

---
+++ python/17AdvPython-10.py
@@ -0,0 +1,9 @@
+def main():
+    to_one_hundred = range(101)
+
+    backwards_by_tens = to_one_hundred[::-10]
+    print backwards_by_tens
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/17AdvPython-11.py
@@ -0,0 +1,11 @@
+def main():
+    to_21= range(1,22)
+    odds = to_21[0::2]
+    middle_third = to_21[7:14]
+    print to_21
+    print odds
+    print middle_third
+
+
+if __name__ == "__main__":
+    main()

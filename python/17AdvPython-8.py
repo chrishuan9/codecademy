2598b06919e9369a248c6fa417342dd957bfcb4a
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-01-25 10:57:01 +0100

    added exercise 7 and 8 from chapter 17

---
+++ python/17AdvPython-8.py
@@ -0,0 +1,8 @@
+def main():
+    my_list = range(1, 11) # List of numbers 1 - 10
+
+    # slicing with omitting indices - picks default
+    print my_list[::2]
+
+if __name__ == "__main__":
+    main()
